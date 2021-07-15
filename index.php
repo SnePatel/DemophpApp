a3eab00b917cc50998fcbb633665515285b06ede
SnePatel <46272377+SnePatel@users.noreply.github.com>
2021-07-15 13:19:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p><h1>Welcome Sne to Webelight Solution</h1></p>'; ?> 
+ <?php echo '<p><h1>Welcome to Webelight Solution</h1></p>'; ?> 
  </body>
 </html>
